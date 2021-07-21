209d6c53d12147bf7bce5c16098a17b4ad0a2be0
Scot <bscot.hagen@gmail.com>
2021-07-21 14:24:03 -0700

    Added data validations stringlength and column name. added migrations

--- ContosoUniversityMVC/Models/Student.cs
+++ ContosoUniversityMVC/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace ContosoUniversityMVC.Models
         [StringLength(50)]
         public string LastName { get; set; }
         [StringLength(50)]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
